dbb1fa8815c32f2de42d3a6a8d0f3e219e5ab576
karthi4660 <karthijeenkhan@gmail.com>
2026-02-20 17:13:27 +0530

    Update fake_sensors.py

--- sensors/fake_sensors.py
+++ sensors/fake_sensors.py
@@ -1,1 +1,10 @@
 
+import random
+
+class FakeSensors:
+    def read_environment(self):
+        return {
+            "noise_level": random.randint(0, 100),
+            "light_level": random.randint(0, 100),
+            "touch_detected": random.choice([True, False])
+        }
