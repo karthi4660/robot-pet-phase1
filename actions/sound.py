0f27f437e488f08df67165407d8a5a40486719e1
karthi4660 <karthijeenkhan@gmail.com>
2026-02-20 17:14:39 +0530

    Update sound.py

--- actions/sound.py
+++ actions/sound.py
@@ -1,1 +1,8 @@
-
+class Sound:
+    def make_sound(self, mood_state):
+        if mood_state == "sad":
+            print("🔊 Beep... (sad tone)")
+        elif mood_state == "excited":
+            print("🔊 Beep Beep! (excited tone)")
+        else:
+            print("🔊 Beep.")
