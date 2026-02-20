46f8a56cbd2abd520d313f4b0a67f4c3da1df69f
karthi4660 <karthijeenkhan@gmail.com>
2026-02-20 17:13:55 +0530

    Update movement.py

--- actions/movement.py
+++ actions/movement.py
@@ -1,1 +1,11 @@
 
+class Movement:
+    def execute(self, action):
+        if action == "sleep":
+            print("🤖 Robot is going to sleep...")
+        elif action == "play":
+            print("🤖 Robot is playing happily!")
+        elif action == "seek_attention":
+            print("🤖 Robot is seeking attention...")
+        else:
+            print("🤖 Robot is idling...")
